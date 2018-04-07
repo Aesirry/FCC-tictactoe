833876d8ce9e1f3c3d46204c92c2f94a97554f4b
Joe Beltrami <jpbeltrami@gmail.com>
2018-04-07 13:44:39 +0800

    adds time delays and reset game upon max turns reached

--- script.js
+++ script.js
@@ -2,57 +2,96 @@
 // User Story: My game will reset as soon as it's over so I can play again.
 // User Story: I can choose whether I want to play as X or O.
 
+// TODO the game doesn't finish
+  // TODO the game resets at 9 or more turns
+  // TODO the game ends when a player with and resets
 // TODO: symbols can be entered twice.. both the same or different
 // TODO: symbols can be out of the game box
 
+// try passing the game to the move function. 
+
+var game;
+var huPlayer;
+var aiPlayer;
+var box_id;
+var turn_count;
+var choices;
+var max;
+var min;
+var result;
+
 $(document).ready(function(){
+  resetGame();
   huPlayer = "x";
   aiPlayer = "o";
+  game = [0,1,2,3,4,5,6,7,8];
 
   $(".game-box").click(function(){
     move(huPlayer, $(this).attr('id'));
     move(aiPlayer, aiChoice(game));
-    if (winning(game, huPlayer)){
-        resetGame();
+    if (turn_count >= 9){
+      console.log('board full');
+      resetGame();
     }
-    console.log(game);
+    if (winning(game, huPlayer)){
+      console.log('player wins');
+        resetGame(1000);
+    }
+    if (winning(game, aiPlayer)){
+      console.log('ai wins');
+      resetGame(1000);
+    }
   });
 
   $("#reset").click(function(){
     resetGame();
   });
-  // while the winning state isn't true keep setting the 
-
 });
 
-function resetGame(){
-  $(".game-box").empty();
-  game = [0,1,2,3,4,5,6,7,8];
+function resetGame(time){
+  setTimeout(function(){
+    $(".game-box").empty();
+    turn_count = 0;
+    game = [0,1,2,3,4,5,6,7,8];
+    return game;
+  }, time);
 }
 
 function aiChoice(board){
   // get the game array with the available spaces
-  let choices = emptyIndexes(game);
-  let max = board.length;
-  let min = 0;
-  return Math.floor(Math.random() * (max + 1));
+  choices = emptyIndexes(game);
+  console.log(choices);
+  max = choices.length;
+  min = 0;
+  result = Math.floor(Math.random() * (max + 1));
+  return result;
 }
 
 function move(player, box_id){
-  if (player === "x"){
-    $("#" + box_id).append('<span class="fa fa-times x-ingame"></span>');
-    return game[box_id] = "x";
-  }
-  else  {
-    $("#" + box_id).append('<span class="fa fa-circle-o o-ingame"></span>');
-    return game[box_id] = "o";
+  turn_count += 1;
+  if (player == aiPlayer){
+      setTimeout(function(){
+        if (player === "x"){
+          $("#" + box_id).append('<span class="fa fa-times x-ingame"></span>');
+          return game[box_id] = "x";
+        }
+        else  {
+          $("#" + box_id).append('<span class="fa fa-circle-o o-ingame"></span>');
+          return game[box_id] = "o";
+        }
+      }, 200)
+    }
+  else {
+    if (player === "x"){
+      $("#" + box_id).append('<span class="fa fa-times x-ingame"></span>');
+      return game[box_id] = "x";
+    }
+    else  {
+      $("#" + box_id).append('<span class="fa fa-circle-o o-ingame"></span>');
+      return game[box_id] = "o";
+    }
   }
 }
-
-var game = [0,1,2,3,4,5,6,7,8];
-var huPlayer;
-var aiPlayer;
-var box_id;
 
 // returns list of empty indexes
 function emptyIndexes(board){
@@ -70,7 +109,7 @@ function winning(board, player){
     (board[2] == player && board[5] == player && board[8] == player) ||
     (board[2] == player && board[4] == player && board[6] == player) ||
     (board[0] == player && board[4] == player && board[8] == player)
-  ) {
+  ){
     return true;
   }
   else {
